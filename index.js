7cf6886e2bb790e41890726be7571b85d08b1d96
Your Name <you@example.com>
2020-09-26 13:39:36 +0700

    Evaluasi

---
+++ index.js
@@ -0,0 +1,5 @@
+function hitungluaspersegipanjang(panjang,lebar) {
+    return panjang * lebar;
+}
+
+document.write('Panjang : 8' + '<br>' + 'Lebar : 5' + '<p>' + 'Luas Persegi Panjang : ' + hitungluaspersegipanjang(8,5));
